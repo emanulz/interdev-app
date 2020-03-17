35af735ac8d07c6d7dfc0dc0e3907fd9d4635066
Emanuel Zuniga <emanuelziga@gmail.com>
2020-03-17 07:11:27 -0600

    Print multiplage pdfs added

--- lib/utils/print_document.dart
+++ lib/utils/print_document.dart
@@ -10,7 +10,7 @@ import 'package:printing/printing.dart';
 class PrintDocument{
 
   //Define your method
-  void printSale (Sale sale, Local local, TaxPayer taxPayer, invoice, ticket) {
+  void printOldSaleDEPRECATED (Sale sale, Local local, TaxPayer taxPayer, invoice, ticket) {
 
     List<int> buildPdf(PdfPageFormat format) {
     final pdf.Document doc = pdf.Document();
@@ -18,13 +18,6 @@ class PrintDocument{
       pdf.Page(
         pageTheme: pdf.PageTheme(theme: pdf.Theme(defaultTextStyle: pdf.TextStyle(fontSize: 9.0)), pageFormat:format),
         build: (pdf.Context context) {
-          // return pdf.ConstrainedBox(
-          //   constraints: const pdf.BoxConstraints.expand(),
-          //   child: pdf.FittedBox(
-          //     child: _buildSalePdf(sale, local, taxPayer, invoice, ticket),
-          //     alignment: pdf.Alignment.topLeft
-          //   ),
-          // );
           return pdf.ConstrainedBox(
             constraints: const pdf.BoxConstraints.expand(),
             child: pdf.Container(
@@ -48,7 +41,7 @@ class PrintDocument{
   }
 
   //Define your method
-  void printPresale (Presale presale, Local local, TaxPayer taxPayer) {
+  void printOldPresaleDEPRECATED (Presale presale, Local local, TaxPayer taxPayer) {
 
     List<int> buildPdf(PdfPageFormat format) {
     final pdf.Document doc = pdf.Document();
@@ -56,13 +49,6 @@ class PrintDocument{
       pdf.Page(
         pageTheme: pdf.PageTheme(theme: pdf.Theme(defaultTextStyle: pdf.TextStyle(fontSize: 9.0)), pageFormat:format),
         build: (pdf.Context context) {
-          // return pdf.ConstrainedBox(
-          //   constraints: const pdf.BoxConstraints.expand(),
-          //   child: pdf.FittedBox(
-          //     child: _buildSalePdf(sale, local, taxPayer, invoice, ticket),
-          //     alignment: pdf.Alignment.topLeft
-          //   ),
-          // );
           return pdf.ConstrainedBox(
             constraints: const pdf.BoxConstraints.expand(),
             child: pdf.Container(
@@ -86,8 +72,53 @@ class PrintDocument{
   }
 
   // ### DOCS TO PRINT ###
+  void printPresale (Presale presale, Local local, TaxPayer taxPayer) {
 
-  pdf.Widget _buildSalePdf(Sale sale, Local local, TaxPayer taxPayer, invoice, ticket) {
+    List<int> buildPdf(PdfPageFormat format) {
+    final pdf.Document doc = pdf.Document();
+    doc.addPage(
+      pdf.MultiPage(
+        pageTheme: pdf.PageTheme(theme: pdf.Theme(defaultTextStyle: pdf.TextStyle(fontSize: 9.0)), pageFormat:format),
+        crossAxisAlignment: pdf.CrossAxisAlignment.center,
+        build: (pdf.Context context) {
+          return _buildNewPresalePdf(presale, local, taxPayer);         
+        },
+      ),
+    );
+
+    return doc.save();
+  }
+
+    Printing.layoutPdf(
+      onLayout: buildPdf,
+    );
+
+  }
+
+  void printSale (Sale sale, Local local, TaxPayer taxPayer, invoice, ticket) {
+
+    List<int> buildPdf(PdfPageFormat format) {
+    final pdf.Document doc = pdf.Document();
+    doc.addPage(
+      pdf.MultiPage(
+        pageTheme: pdf.PageTheme(theme: pdf.Theme(defaultTextStyle: pdf.TextStyle(fontSize: 9.0)), pageFormat:format),
+        crossAxisAlignment: pdf.CrossAxisAlignment.center,
+        build: (pdf.Context context) {
+          return _buildNewSalePdf(sale, local, taxPayer, invoice, ticket);         
+        },
+      ),
+    );
+
+    return doc.save();
+  }
+
+    Printing.layoutPdf(
+      onLayout: buildPdf,
+    );
+
+  }
+
+  pdf.Widget _buildSalePdfDEPRECATED(Sale sale, Local local, TaxPayer taxPayer, invoice, ticket) {
     
     final String idText = taxPayer.idType == '02' ? 'Céd Jurid No' : 'Céd No';
     final String type = invoice != null ? 'Factura Electrónica' : ticket != null ? 'Tiquete Electronico' : 'No Encontrado';
@@ -148,7 +179,7 @@ class PrintDocument{
     ]);
   }
 
-  pdf.Widget _buildPresalePdf(Presale presale, Local local, TaxPayer taxPayer) {
+  pdf.Widget _buildPresalePdfDEPRECATED(Presale presale, Local local, TaxPayer taxPayer) {
     
     final String idText = taxPayer.idType == '02' ? 'Céd Jurid No' : 'Céd No';
     final String type = presale.presaleType == 'REGULAR' ? 'PREVENTA' : presale.presaleType == 'RESERVE' ? 'RECIBO' : presale.presaleType == 'QUOTING' ? 'PROFORMA' : 'PREVENTA';
@@ -204,6 +235,120 @@ class PrintDocument{
     ]);
   }
 
+  List<pdf.Widget> _buildNewPresalePdf(Presale presale, Local local, TaxPayer taxPayer) {
+    
+    final String idText = taxPayer.idType == '02' ? 'Céd Jurid No' : 'Céd No';
+    final String type = presale.presaleType == 'REGULAR' ? 'PREVENTA' : presale.presaleType == 'RESERVE' ? 'RECIBO' : presale.presaleType == 'QUOTING' ? 'PROFORMA' : 'PREVENTA';
+    // TODO: IMPLEMENT CURRENCY SYMBOL WITH OTHER FONT
+    // final String currencySymbol = sale.currencyCode == 'CRC' ? '₡' : sale.currencyCode == 'USD' ? '\$' : 'N';
+    final String currencySymbol = 'C ';
+
+    final List<pdf.Widget> table = List<pdf.Widget>.generate(presale.cart.cartItems.length, (int index){
+      return generateTableRow(presale.cart.cartItems[index], currencySymbol);
+    });
+    
+    return <pdf.Widget> [
+      // BLANK SPACE
+      pdf.SizedBox(height: 10.0),
+      // HEADER
+      pdf.Center(child: pdf.Text(local.name, style: pdf.TextStyle(fontSize: 10.0))),
+      pdf.Center(child: pdf.Text(taxPayer.legalName)),
+      pdf.Center(child: pdf.Text('$idText ${taxPayer.idNumber}')),
+      pdf.Center(child: pdf.Text(local.receiptAddress)),
+      pdf.Center(child: pdf.Text(local.longAddress, style: pdf.TextStyle(fontSize: 8.0))),
+      pdf.Center(child: pdf.Text('Tel: ${local.phoneNumber}')),
+      pdf.Center(child: pdf.Text(local.email)),
+      // BLANK SPACE
+      pdf.SizedBox(height: 10.0),
+      // SEPARATOR
+      generateSeparator('RECIBO DE VENTA'),
+      // BLANK SPACE
+      pdf.SizedBox(height: 10.0),
+      // DATA
+      // generateFlexibleRow('Fecha:', '22/10/2019 03:06:48 pm'),
+      generateFlexibleRow('Tipo:', type),
+      generateFlexibleRow('Preventa:', presale.consecutive.toString()),
+      generateFlexibleRow('Cliente:', '${presale.client.code} - ${presale.client.name} ${presale.client.lastName}'),
+
+      pdf.SizedBox(height: 10.0),
+
+      generateTableHeader(),
+      generateSeparatorLine(),
+
+      pdf.Wrap(children: table),
+
+      pdf.SizedBox(height: 10.0),
+      //TOTALS
+      // generateSubTotalsRow('Subtotal', presale.cart.cartSubtotalNoDiscount.toStringAsFixed(2), currencySymbol),
+      generateSubTotalsRow('Descuento', presale.cart.discountTotal.toStringAsFixed(2), currencySymbol),
+      // generateSubTotalsRow('IVA', presale.cart.cartTaxes.toStringAsFixed(2), currencySymbol),
+
+      generateSubtotalsSeparator(),
+
+      generateSubTotalsRow('Total', presale.cart.cartTotal.toStringAsFixed(2), currencySymbol),
+      
+      pdf.SizedBox(height: 10.0),
+
+    ];
+  }
+
+  List<pdf.Widget> _buildNewSalePdf(Sale sale, Local local, TaxPayer taxPayer, invoice, ticket) {
+    
+    final String idText = taxPayer.idType == '02' ? 'Céd Jurid No' : 'Céd No';
+    final String type = invoice != null ? 'Factura Electrónica' : ticket != null ? 'Tiquete Electronico' : 'No Encontrado';
+    final String consetuveNumbering = invoice != null ? invoice.consecutiveNumbering : ticket != null ? ticket.consecutiveNumbering : 'No Encontrado';
+    final String numericKey = invoice != null ? '${invoice.numericKey.substring(0,21)} ${invoice.numericKey.substring(21)}' : ticket != null ? ticket.numericKey : 'No Encontrado';
+    // TODO: IMPLEMENT CURRENCY SYMBOL WITH OTHER FONT
+    // final String currencySymbol = sale.currencyCode == 'CRC' ? '₡' : sale.currencyCode == 'USD' ? '\$' : 'N';
+    final String currencySymbol = 'C ';
+    final List<pdf.Widget> table = List<pdf.Widget>.generate(sale.cart.cartItems.length, (int index){
+      return generateTableRow(sale.cart.cartItems[index], currencySymbol);
+    });
+    
+    return <pdf.Widget> [
+      pdf.Center(child: pdf.Text(local.name, style: pdf.TextStyle(fontSize: 10.0))),
+      pdf.Center(child: pdf.Text(taxPayer.legalName)),
+      pdf.Center(child: pdf.Text('$idText ${taxPayer.idNumber}')),
+      pdf.Center(child: pdf.Text(local.receiptAddress)),
+      pdf.Center(child: pdf.Text(local.longAddress, style: pdf.TextStyle(fontSize: 8.0))),
+      pdf.Center(child: pdf.Text('Tel: ${local.phoneNumber}')),
+      pdf.Center(child: pdf.Text(local.email)),
+      // BLANK SPACE
+      pdf.SizedBox(height: 10.0),
+      // SEPARATOR
+      generateSeparator('FACTURA DE CONTADO'),
+      // BLANK SPACE
+      pdf.SizedBox(height: 10.0),
+      // DATA
+      // generateFlexibleRow('Fecha:', '22/10/2019 03:06:48 pm'),
+      generateFlexibleRow('Tipo:', type),
+      generateFlexibleRow('Factura:', sale.consecutive.toString()),
+      generateFlexibleRow('Consec:', consetuveNumbering),
+      generateFlexibleRow('Clave:', numericKey),
+      generateFlexibleRow('Cliente:', '${sale.client.code} - ${sale.client.name} ${sale.client.lastName}'),
+
+      pdf.SizedBox(height: 10.0),
+      
+      // TABLE
+      generateTableHeader(),
+      generateSeparatorLine(),
+      pdf.Wrap(children: table),
+      
+      pdf.SizedBox(height: 10.0),
+      //TOTALS
+      generateSubTotalsRow('Subtotal', sale.cart.cartSubtotalNoDiscount.toStringAsFixed(2), currencySymbol),
+      generateSubTotalsRow('Descuento', sale.cart.discountTotal.toStringAsFixed(2), currencySymbol),
+      generateSubTotalsRow('IVA', sale.cart.cartTaxes.toStringAsFixed(2), currencySymbol),
+
+      generateSubtotalsSeparator(),
+
+      generateSubTotalsRow('Total', sale.cart.cartTotal.toStringAsFixed(2), currencySymbol),
+      
+      pdf.SizedBox(height: 10.0),
+      generateHaciendaStatementRow(),
+
+    ];
+  }
 
   // ### AUX METHOS FOR BUILDING PDF SECTIONS ###
 
